6b0e52ad6b8199d0c6e969d39e830b00637c590c
Marcin Ciesla <marcin-ciesla@live.com>
2022-12-20 19:29:23 +0100

    day 19

--- day19.cs
+++ day19.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.ComponentModel;
+using System.Net;
 using System.Text.RegularExpressions;
 using System.Xml.Xsl;
 
@@ -8,276 +9,168 @@ namespace adwent2022
     {
         static StreamReader GetInput()
         {
-            //return File.OpenText("inputs/19.txt");
-            return File.OpenText("inputs/example.txt");
+            return File.OpenText("inputs/19.txt");
+            //return File.OpenText("inputs/example.txt");
         }
 
         public static void Part1()
         {
             var input = GetInput().ReadToEnd().Split('\n');
 
-            var blueprints = new List<Blueprint>();
+            var blueprints = new List<int[][]>();
             foreach (var line in input)
             {
                 var bp = Regex.Matches(line, @"\d+");
-                blueprints.Add(new Blueprint(
-                    int.Parse(bp[0].Value),
-                    int.Parse(bp[1].Value),
-                    int.Parse(bp[2].Value),
-                    int.Parse(bp[3].Value),
-                    int.Parse(bp[4].Value),
-                    int.Parse(bp[5].Value),
-                    int.Parse(bp[6].Value)));
+                blueprints.Add(new int[][]
+                {
+                    new int[]{int.Parse(bp[1].Value), 0, 0, 0},
+                    new int[]{int.Parse(bp[2].Value), 0, 0, 0},
+                    new int[]{int.Parse(bp[3].Value), int.Parse(bp[4].Value), 0, 0 },
+                    new int[]{int.Parse(bp[5].Value), 0, int.Parse(bp[6].Value), 0},
+                });
             }
-            
+
             var qualitySum = 0;
-            foreach (var blueprint in blueprints)
+
+            for (int bp = 0; bp < 3; bp++)
             {
-                var robots = new Resources(1, 0, 0, 0);
-                var resources = new Resources(0, 0, 0, 0);
-                var timeLeft = 24;
-                var maxGeodeYield = Simulate(blueprint, robots, resources, timeLeft, null);
-                Console.WriteLine($"blueprint: {blueprint.BlueprintId}; max geode yeld {maxGeodeYield}");
-                qualitySum += (maxGeodeYield * blueprint.BlueprintId);
+                var robots = new int[] { 1, 0, 0, 0 };
+                var resources = new int[] { 0, 0, 0, 0 };
+                var maxCost = new int[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    maxCost[i] = blueprints[bp].Max(e => e[i]);
+                }
+                maxCost[3] = int.MaxValue;
+                var timeLeft = 32;
+                var toCheck = new Queue<Simulation>();
+                var maxYield = 0;
+                toCheck.Enqueue(new Simulation
+                {
+                    Resources = resources,
+                    Robots = robots,
+                    MaxCost = maxCost,
+                    TimeLeft = timeLeft,
+                    PendingBuild = -1
+                });
+
+                while (toCheck.Count > 0)
+                {
+                    var yield = Simulate(toCheck.Dequeue(), blueprints[bp], toCheck);
+                    maxYield = maxYield < yield ? yield : maxYield;
+                }
+
+                Console.WriteLine($"blueprint: {bp + 1}; max geode yeld {maxYield}");
+                qualitySum += (maxYield * (bp + 1));
             }
-            
+
             Console.WriteLine($"got {qualitySum} quality total");
         }
 
+        private static int Simulate(Simulation simulation, int[][] blueprint, Queue<Simulation> toCheck)
+        {
+            if (simulation.TimeLeft == 0)
+            {
+                return simulation.Resources[3];
+            }
+            if (simulation.TimeLeft < 0)
+            {
+                return 0;
+            }
+
+            simulation.TimeLeft = simulation.TimeLeft - 1;
+
+            for (int i = 0; i < 4; i++)
+            {
+                simulation.Resources[i] = simulation.Resources[i] + simulation.Robots[i];
+            }
+
+            if (simulation.PendingBuild != -1)
+            {
+                ++simulation.Robots[simulation.PendingBuild];
+                for (int i = 0; i < 4; i++)
+                {
+                    simulation.Resources[i] = simulation.Resources[i] - blueprint[simulation.PendingBuild][i];
+                }
+            }
+            var noNewRobot = true;
+            for (int i = 3; i > -1; i--)
+            {
+                //var build = simulation.MaxCost[i] * simulation.TimeLeft > (simulation.Robots[i] * simulation.TimeLeft) + simulation.Resources[i];
+                var build = simulation.MaxCost[i] > simulation.Robots[i];
+
+                if (!build) continue;
+
+                build = blueprint[i][0] <= simulation.Resources[0] &&
+                        blueprint[i][1] <= simulation.Resources[1] &&
+                        blueprint[i][2] <= simulation.Resources[2];
+
+                if (build)
+                {
+                    simulation.PendingBuild = i;
+                    simulation.Robots = simulation.Robots.ToArray();
+                    simulation.Resources = simulation.Resources.ToArray();
+                    toCheck.Enqueue(simulation);
+                    noNewRobot = false;
+                    if (i == 3)
+                    {
+                        break;
+                    }
+                    continue;
+                }
+
+                for (int j = 0; j < 3; j++)
+                {
+                    build = blueprint[i][j] == 0 || simulation.Robots[j] > 0;
+                    if (!build) break;
+                }
+
+                if (build)
+                {
+                    var ff = 0;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        while (blueprint[i][j] > simulation.Resources[j] + (simulation.Robots[j] * ff))
+                        {
+                            ff++;
+                        }
+                    }
+                    if (simulation.TimeLeft - ff >= 0)
+                    {
+                        toCheck.Enqueue(new Simulation
+                        {
+                            MaxCost = simulation.MaxCost,
+                            PendingBuild = i,
+                            Resources = simulation.Resources.Select((e, i) => e + (simulation.Robots[i] * ff)).ToArray(),
+                            Robots = simulation.Robots.ToArray(),
+                            TimeLeft = simulation.TimeLeft - ff
+                        });
+                        noNewRobot = false;
+                    }
+                }
+            }
+            if (noNewRobot)
+            {
+                simulation.PendingBuild = -1;
+                simulation.Robots = simulation.Robots.ToArray();
+                simulation.Resources = simulation.Resources.ToArray();
+                toCheck.Enqueue(simulation);
+            }
+            return 0;
+        }
 
         public static void Part2()
         {
 
         }
-
-        static int Simulate(Blueprint blueprint, Resources robots, Resources resources, int timeLeft, Type? robotToBuild)
-        {
-            if (timeLeft == 0)
-                return resources.Geode;
-
-            //copy objects
-            var localRobots = (Resources)robots.Clone();
-            var localResources = (Resources)resources.Clone();
-
-            //yeld materials
-            localResources.Add(localRobots);
-
-            //build robot if available
-            if (robotToBuild != null)
-            {
-                localRobots.Add(blueprint.BuildRobot(localResources, robotToBuild, out localResources));
-            }
-
-            var availableRobots = blueprint.RobotsBuildable(localResources);
-            var ifWait = Simulate(blueprint, localRobots, localResources, timeLeft - 1, null);
-            if (!availableRobots.Any()) return ifWait;
-            var maxSimulated = availableRobots.Select(e =>Simulate(blueprint, localRobots, localResources, timeLeft - 1, e)).Max();
-            return Math.Max(ifWait, maxSimulated);
-        }
     }
 
-    class Blueprint
+    struct Simulation
     {
-        public Blueprint(
-            int blueprintId,
-            int oreRobotOreCost,
-            int clayRobotOreCost,
-            int obsidianRobotOreCost,
-            int obsidianRobotClayCost,
-            int geodeRobotOreCost,
-            int geodeRobotObsidianCost)
-        {
-            BlueprintId = blueprintId;
-            OreRobot = new OreRobot(new Resources(oreRobotOreCost, 0, 0, 0));
-            ClayRobot = new ClayRobot(new Resources(clayRobotOreCost, 0, 0, 0));
-            ObsidianRobot = new ObsidianRobot(new Resources(obsidianRobotOreCost, obsidianRobotClayCost, 0, 0));
-            GeodeRobot = new GeodeRobot(new Resources(geodeRobotOreCost, 0, geodeRobotObsidianCost, 0));
-        }
-        public int BlueprintId { get; set; }
-        public OreRobot OreRobot { get; set; }
-        public ClayRobot ClayRobot { get; set; }
-        public ObsidianRobot ObsidianRobot { get; set; }
-        public GeodeRobot GeodeRobot { get; set; }
-
-        public List<Type> RobotsBuildable(Resources resources)
-        {
-            var retList = new List<Type>();
-            if (OreRobot.CanBeBuilt(resources))
-            {
-                retList.Add(typeof(OreRobot));
-            }
-            if (ClayRobot.CanBeBuilt(resources))
-            {
-                retList.Add(typeof(ClayRobot));
-            }
-            if (ObsidianRobot.CanBeBuilt(resources))
-            {
-                retList.Add(typeof(ObsidianRobot));
-            }
-            if (GeodeRobot.CanBeBuilt(resources))
-            {
-                retList.Add(typeof(GeodeRobot));
-            }
-            return retList;
-        }
-
-        public Resources BuildRobot(Resources resources, Type robotType, out Resources resourcesLeft)
-        {
-            if (robotType == OreRobot.GetType())
-            {
-                return OreRobot.Build(resources, out resourcesLeft);
-            }
-            else if (robotType == ClayRobot.GetType())
-            {
-                return ClayRobot.Build(resources, out resourcesLeft);
-            }
-            else if (robotType == ObsidianRobot.GetType())
-            {
-                return ObsidianRobot.Build(resources, out resourcesLeft);
-            }
-            else if (robotType == GeodeRobot.GetType())
-            {
-                return GeodeRobot.Build(resources, out resourcesLeft);
-            }
-            else
-            {
-                throw new Exception("unsupported");
-            }
-        }
-    }
-
-    abstract class Robot
-    {
-        public Robot(Resources cost)
-        {
-            Cost = cost;
-        }
-
-        public Resources Cost { get; set; }
-        public bool CanBeBuilt(Resources resources)
-        {
-            return Cost.Ore <= resources.Ore
-                && Cost.Clay <= resources.Clay
-                && Cost.Obsidian <= resources.Obsidian
-                && Cost.Geode <= resources.Geode;
-        }
-        public abstract Resources Build(Resources resources, out Resources resourcesLeft);
-    }
-
-    class OreRobot : Robot
-    {
-        public OreRobot(Resources cost) : base(cost)
-        {
-        }
-
-        public override Resources Build(Resources resources, out Resources resourcesLeft)
-        {
-            if (CanBeBuilt(resources))
-            {
-                resourcesLeft = resources.Spend(Cost);
-                return new Resources(1, 0, 0, 0);
-            }
-            else
-            {
-                resourcesLeft = resources;
-                return new Resources(0, 0, 0, 0);
-            }
-        }
-    }
-    class ClayRobot : Robot
-    {
-        public ClayRobot(Resources cost) : base(cost)
-        {
-        }
-
-        public override Resources Build(Resources resources, out Resources resourcesLeft)
-        {
-            if (CanBeBuilt(resources))
-            {
-                resourcesLeft = resources.Spend(Cost);
-                return new Resources(0, 1, 0, 0);
-            }
-            else
-            {
-                resourcesLeft = resources;
-                return new Resources(0, 0, 0, 0);
-            }
-        }
-    }
-    class ObsidianRobot : Robot
-    {
-        public ObsidianRobot(Resources cost) : base(cost)
-        {
-        }
-
-        public override Resources Build(Resources resources, out Resources resourcesLeft)
-        {
-            if (CanBeBuilt(resources))
-            {
-                resourcesLeft = resources.Spend(Cost);
-                return new Resources(0, 0, 1, 0);
-            }
-            else
-            {
-                resourcesLeft = resources;
-                return new Resources(0, 0, 0, 0);
-            }
-        }
-    }
-    class GeodeRobot : Robot
-    {
-        public GeodeRobot(Resources cost) : base(cost)
-        {
-        }
-
-        public override Resources Build(Resources resources, out Resources resourcesLeft)
-        {
-            if (CanBeBuilt(resources))
-            {
-                resourcesLeft = resources.Spend(Cost);
-                return new Resources(0, 0, 0, 1);
-            }
-            else
-            {
-                resourcesLeft = resources;
-                return new Resources(0, 0, 0, 0);
-            }
-        }
-    }
-
-    class Resources : ICloneable
-    {
-        public Resources(int ore, int clay, int obsidian, int geode)
-        {
-            Ore = ore;
-            Clay = clay;
-            Obsidian = obsidian;
-            Geode = geode;
-        }
-        public int Ore { get; set; }
-        public int Clay { get; set; }
-        public int Obsidian { get; set; }
-        public int Geode { get; set; }
-        public Resources Add(Resources resources)
-        {
-            Ore += resources.Ore;
-            Clay += resources.Clay;
-            Obsidian += resources.Obsidian;
-            Geode += resources.Geode;
-            return this;
-        }
-        public Resources Spend(Resources resources)
-        {
-            Ore -= resources.Ore;
-            Clay -= resources.Clay;
-            Obsidian -= resources.Obsidian;
-            Geode -= resources.Geode;
-            return this;
-        }
-
-        public object Clone()
-        {
-            return new Resources(Ore, Clay, Obsidian, Geode);
-        }
+        public int[] Robots;
+        public int[] Resources;
+        public int[] MaxCost;
+        public int PendingBuild;
+        public int TimeLeft;
     }
 }
